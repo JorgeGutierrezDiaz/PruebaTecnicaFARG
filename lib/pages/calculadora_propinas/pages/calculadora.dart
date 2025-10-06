c4c07e5b9434200893e836900be1ca051db72fc2
Jorge Gutierrezx <jorge.gutierrez@getechnologies.com.mx>
2025-10-06 13:03:33 -0600

    add estructura inicial

---
+++ lib/pages/calculadora_propinas/pages/calculadora.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculadoraPropinas extends StatefulWidget {
+  const CalculadoraPropinas({super.key});
+
+  @override
+  State<CalculadoraPropinas> createState() => _CalculadoraPropinasState();
+}
+
+class _CalculadoraPropinasState extends State<CalculadoraPropinas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
